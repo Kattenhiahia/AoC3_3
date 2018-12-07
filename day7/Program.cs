74e6c6ac33b29cabe34022b2536379027e5d3340
Samuel Häggmyr <samuel.haggmyr@afconsult.com>
2018-12-07 06:41:11 +0100

    day 7 stommen

---
+++ day7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = System.IO.File.ReadAllText(@"C:\Users\samue\source\repos\AoC18_2\day7\test.txt");
+
+			//string input = "dabAcCaCBAcCcaDA";
+
+			Console.WriteLine($"input: {input.Length}");
+			part1(input);
+			Console.WriteLine("Press any key to exit.");
+			System.Console.ReadKey();
+		}
+		static void part1(string input)
+		{
+			Dictionary<string, int> instructions = new Dictionary<string, int>();
+			var inp = input.Split('\n');
+			foreach (string row in inp)
+			{
+
+				Console.WriteLine($"first: {row[5]}, 2nd: {row[36]}");
+				if (instructions.ContainsKey(row[5].ToString()))
+				{
+
+				}
+				else if (instructions.ContainsKey(row[36].ToString()))
+				{
+
+				}
+				else
+				{
+
+				}
+			}
+		}
+	}
+}
